5f4dcb56bef5f81173287a0e75f0998d6e191be0
abdou <abderrahmentalby@gmail.com>
2022-05-07 04:19:42 +0100

    trigleet

--- src/app/compnment/accueil/accueil.component.ts
+++ src/app/compnment/accueil/accueil.component.ts
@@ -1,9 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-<<<<<<< HEAD
 import { PostService } from 'src/app/services/post.service';
-=======
-import { PostService } from 'src/app/service/post.service';
->>>>>>> ccaa32c91ae274e7c0d4b08e0dcf8f33a12cfbba
 
 @Component({
   selector: 'app-accueil',
@@ -11,7 +7,6 @@ import { PostService } from 'src/app/service/post.service';
   styleUrls: ['./accueil.component.css']
 })
 export class AccueilComponent implements OnInit {
-<<<<<<< HEAD
   posts:any = []
   post : any;
   constructor(private service : PostService) { }
@@ -42,22 +37,9 @@ export class AccueilComponent implements OnInit {
       this.getPosts()
 
     }, err => console.log(err))
-=======
-  allpost!:any[];
-  constructor(private service:PostService) { }
-
-  ngOnInit(): void {
-    this.getAllPost()
->>>>>>> ccaa32c91ae274e7c0d4b08e0dcf8f33a12cfbba
   }
-getAllPost(){
- this.service.getAllPosts().subscribe(
-   res=>{
-     this.allpost=res;
-   }
- )
+ 
 
-<<<<<<< HEAD
   getPosts(){
     this.service.getAllPosts().pipe().subscribe(
       data=> {this.posts = data 
@@ -65,10 +47,7 @@ getAllPost(){
       err =>console.log(err)
     )
   }
-=======
- }
->>>>>>> ccaa32c91ae274e7c0d4b08e0dcf8f33a12cfbba
 }
-
-
+ 
+ 
 
